3ced8c117926f97b1cce769abb03919300807fee
v1x1e <github@victoriahchang.com>
2017-11-18 23:21:17 -0800

    Fix HTML and export

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export default Event = (props) => {
+const Event = (props) => {
   return (
-    <div className="event container">
-      <img id="event-img" src=""/>
-      <div id="event-name">Event Name</div>
-      <div id="event-description">This is the event description.</div>
+    <div className="event list-item container column">
+      <img className="list-item-img event-img" src="http://cdn1.theodysseyonline.com/files/2015/12/04/635848643983265445-579762_christmas-puppy-pictures-tzulxzui.jpg"/>
+      <div className="list-item-name event-name">Puppy Christmas</div>
+      <div className="list-item-description event-description">This is the event description.</div>
     </div>
   )
 };
+
+export default Event;
