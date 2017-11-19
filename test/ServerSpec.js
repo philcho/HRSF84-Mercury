4823113e65e5fa74164a2364222ee2fc00270c0c
Alan Chin <chinalanq@gmail.com>
2017-11-18 16:40:27 -0800

    Add CircleCI config

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -4,6 +4,11 @@ const server = require('../server');
 const db = require('../db');
 
 describe('', () => {
-
+  describe('Empty Test:', () => {
+    it('1 should be a number', (done) => {
+      expect(typeof 1).to.equal('number');
+      done();
+    }); 
+  });
 });
 
