3e25858247274c1dbe629b964a59c26eb21b593e
v1x1e <github@victoriahchang.com>
2017-11-18 23:24:21 -0800

    Add Events components, html, and loadEvents

---
+++ src/loadEvents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Events from './components/Events.jsx';
+
+document.addEventListener('DOMContentLoaded', function () {
+  // load the template component on the template.html page
+  ReactDOM.render(
+    <Events/>,
+    document.getElementById('events')
+  )
+});
