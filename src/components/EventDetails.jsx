09563ba9452ffaa2a6105b0973c67d851b7b01e3
philcho <Phil Cho>
2017-11-20 13:49:13 -0800

    Create Event Details files based on Profile files.

---
+++ src/components/EventDetails.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class EventDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eventDetailsData: {}
+    }
+  }
+
+  getEventDetailsData() {
+    // Example URL: http://localhost:3000/event/5a0ddac3b218d0cadf73eefb
+    let eventId = window.location.href.split('/event/')[1];
+    console.log('eventId', eventId);
+
+    axios.post('/getParticularEvent', {
+      eventInfo: {
+        _id: eventId
+      }
+    })
+    .then(function (response) {
+      this.setState({ eventDetailsData: response.data[0] });
+    }.bind(this))
+    .catch(function (error) {
+      console.log('getEventDetailsData error', error);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>name: {this.state.eventDetailsData.name}</li>
+          <li>date: {this.state.eventDetailsData.date}</li>
+          <li>picture: {/* TODO: Add to schema. this.state.eventDetailsData.picture */}</li>
+          <li>description: {this.state.eventDetailsData.description}</li>
+          <li>comments: 
+            <ol>
+              {/* TODO: Add Comments Component */}
+            </ol>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.getEventDetailsData();
+  }
+}
