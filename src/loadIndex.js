5aabf41419e78e6f48c687332b58fc88d63f6694
v1x1e <github@victoriahchang.com>
2017-11-21 00:01:57 -0800

    (refactor) Use ES6 function definition and JSX component

--- src/loadIndex.js
+++ src/loadIndex.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Template from './components/Index.jsx';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   // load the template component on the template.html page
   ReactDOM.render(
     <Index/>,
--- src/loadProfile.js
+++ src/loadProfile.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './components/Profile.jsx';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   // load the profile component on the profile.html page
   ReactDOM.render(
-    React.createElement(Profile),
+    <Profile/>,
     document.getElementById('profile')
   )
 });
