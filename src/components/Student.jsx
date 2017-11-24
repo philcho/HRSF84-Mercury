721d57147ea81a5467f2a2755c7eb84951c1dcd2
philcho <Phil Cho>
2017-11-24 11:15:58 -0600

    Link each listing on Students page to their Profile page.

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Student = (props) => {
   return (
     <div className="student list-item container column">
-      <img className="list-item-img student-img" src={props.studentData.picture} />
-      <div className="list-item-name student-name">{props.studentData.name}</div>
+      <a className="list-item-a student-a" href={`/profile/${props.studentData._id}`}>
+        <img className="list-item-img student-img" src={props.studentData.picture} />
+        <label className="list-item-name student-name">{props.studentData.name}</label>
+      </a>
     </div>
   )
 };
