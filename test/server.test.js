d2c23c75d2f4a4aaf101fd8294e61b5dfee600d5
Alan Chin <chinalanq@gmail.com>
2017-11-24 15:54:04 -0800

    Remove test for template route

--- test/server.test.js
+++ test/server.test.js
@@ -4,18 +4,6 @@ import request from 'supertest';
 import {app} from '../server';
 
 describe('Server', () => {
-  describe('Template path', () => {
-    it('should respond with status 200 when GET is sent to /template', (done) => {
-      request(app)
-        .get('/template')
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          done();
-        });
-    }); 
-  });
-
   describe('Bundle requests path', () => {
     it('should respond with status 200 when GET is sent to /*.bundle.js', (done) => {
       request(app)
