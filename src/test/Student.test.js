ad815385be385e3e0c9285f3ea22a349eee93fc0
philcho <Phil Cho>
2017-11-24 16:54:52 -0600

    Move Enzyme setup commands into their own, re-usable file.

--- src/test/Student.test.js
+++ src/test/Student.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import Student from '../components/Student';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('Student', () => {
   it('should render correctly', () => {
