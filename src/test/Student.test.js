a50cd49ba192b3b809b8f293681c14d8b8a8eaff
philcho <Phil Cho>
2017-11-24 16:51:04 -0600

    Update test for Student component to include passed-in props.

--- src/test/Student.test.js
+++ src/test/Student.test.js
@@ -9,9 +9,22 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('Student', () => {
   it('should render correctly', () => {
+<<<<<<< HEAD
     const studentData = {};
     const output = shallow(
       <Student studentData={studentData} />
+=======
+    const student = {
+      "_id":"5a0d089080bcabc5ac8cb222",
+      "name":"Frodo Baggins",
+      "picture":"https://www.w3schools.com/w3css/img_fjords.jpg",
+      "bio":"I am from someplace and I did something",
+      "__v":0,
+      "comments":[]
+    };
+    const output = shallow(
+      <Student studentData={student} key={student._id} />
+>>>>>>> Update test for Student component to include passed-in props.
     );
     expect(shallowToJson(output)).toMatchSnapshot();
   });
