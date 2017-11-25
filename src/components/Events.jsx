4f60ee915d34f9c0c0c8926a336a89d11d764fce
v1x1e <github@victoriahchang.com>
2017-11-24 16:14:17 -0800

    Add routes for Events and pass props to Event component

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import Nav from './Nav.jsx';
 import Event from './Event.jsx';
 
 export default class Events extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      events: []
+    };
+  }
+
+  getAllEvents() {
+    axios.get('/getAllEvents')
+      .then((response) => {
+        this.setState({ events: response.data });
+      })
+      .catch((error) => {
+        console.log(`----- getAllEvents error : ${error} -----`);
+      });
+  }
+
+  componentDidMount() {
+    this.getAllEvents();
   }
 
   render() {
@@ -16,9 +33,13 @@ export default class Events extends React.Component {
         <div className="events list container column">
           <h1>EVENTS</h1>
 
-          <Event/>
-          <Event/>
-          <Event/>
+          <div className="container column">
+            {this.state.events.map((event, index) => {
+                return (
+                  <Event event={event} key={event._id}/>
+                );
+              })}
+          </div>
         </div>
       </div>
     );
