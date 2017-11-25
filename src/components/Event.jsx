8bc34adbea33db0fc871009fae73c420c0b5473a
Alan Chin <chinalanq@gmail.com>
2017-11-25 12:25:38 -0800

    Fix route in /event; fix functionality for comments in EventDetail

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Event = (props) => {
   return (
-    <a className="event list-item container column" href={`/eventdetails/${props.event._id}`}>
+    <a className="event list-item container column" href={`/event/${props.event._id}`}>
       <img className="list-item-img event-img" src={props.event.picture}/>
       <div className="list-item-name event-name">{props.event.name}</div>
       <div className="list-item-description event-description">{props.event.description}</div>
