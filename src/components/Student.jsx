fbdec137c94f11f2b7c1dec8dc69f722f0b5e27b
Alan Chin <chinalanq@gmail.com>
2017-11-25 13:07:33 -0800

    Change functional components to spread attributes

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Student = (props) => {
+const Student = ({studentData}) => {
   return (
     <div className="student list-item container column">
-      <a className="list-item-a student-a" href={`/profile/${props.studentData._id}`}>
-        <img className="list-item-img student-img" src={props.studentData.picture} />
-        <label className="list-item-name student-name">{props.studentData.name}</label>
+      <a className="list-item-a student-a" href={`/profile/${studentData._id}`}>
+        <img className="list-item-img student-img" src={studentData.picture} />
+        <label className="list-item-name student-name">{studentData.name}</label>
       </a>
     </div>
   )
