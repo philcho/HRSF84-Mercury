855feca13dc887b0624c49da498a9fad7dd73326
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-26 15:54:25 -0800

    (fix) Fix SuperlativeLink
    The superlative urls are based off of the name, not the id.
    This is much more user friendly.

--- src/components/SuperlativeLink.jsx
+++ src/components/SuperlativeLink.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const SuperlativeLink = ({superlative}) => {
+const SuperlativeLink = ({ superlative }) => {
   return (
-    <a className="superlative-details-info container column" href={`/superlative/${superlative._id}`}>
+    <a className="superlative-details-info container column" href={`/superlative/${superlative.superlative}`}>
       <img className="superlative-picture" src={superlative.img} />
       <div className="container column">
         <div className="superlative-name">{superlative.superlative}</div>
