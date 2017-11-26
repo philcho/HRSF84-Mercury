9ad43f0d99da7f68f046898f1e43a048b387a7ad
v1x1e <github@victoriahchang.com>
2017-11-26 13:50:41 -0800

    Add CSS file for Events and style Events

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Event = ({event}) => {
   return (
-    <a className="event list-item container column" href={`/event/${event._id}`}>
-      <img className="list-item-img event-img" src={event.picture}/>
-      <div className="list-item-name event-name">{event.name}</div>
-      <div className="list-item-description event-description">{event.description}</div>
+    // <a className="event container column" href={`/event/${event._id}`}>
+    //   <img className="list-item-img event-img" src={event.picture}/>
+    //   <div className="list-item-name event-name">{event.name}</div>
+    //   <div className="list-item-bio event-bio">{event.description}</div>
+    // </a>
+
+    <a className="event container column" href="###">
+      <img className="list-item-img event-img" src="http://cdn1.theodysseyonline.com/files/2015/12/04/635848643983265445-579762_christmas-puppy-pictures-tzulxzui.jpg"/>
+      <div className="list-item-name event-name">Puppy Christmas</div>
+      <div className="list-item-bio event-bio">There were puppies and we all played and laughed and danced!</div>
     </a>
   )
 };
