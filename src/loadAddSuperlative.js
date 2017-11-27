55e3c075c60c95a34fa7b17d4fc471c6dea39022
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-27 10:33:02 -0800

    (refactor) Fix some comments

--- src/loadAddSuperlative.js
+++ src/loadAddSuperlative.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AddSuperlative from './components/AddSuperlative.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // load the superlative component on the superlative.html page
   ReactDOM.render(
     <AddSuperlative />,
     document.getElementById('addSuperlative')
