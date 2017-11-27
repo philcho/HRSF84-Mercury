618305847cb53cd4056087cad2654daea6798c46
Alan Chin <chinalanq@gmail.com>
2017-11-27 10:37:12 -0800

    Extend CommentForm component to support shoutouts

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CommentForm = ({onSubmit, onChangeName, onChangeText, name, text}) => {
+const CommentForm = ({onSubmit, onChangeName, onChangeText, onChangeCategory, name, text, category = null}) => {
   const inputStyle = {
     'border': '1px solid black',
     'padding': '5px',
@@ -10,6 +10,16 @@ const CommentForm = ({onSubmit, onChangeName, onChangeText, name, text}) => {
   return (
     <div className="form-comment container">
       <form onSubmit={onSubmit}>
+        { (category !== null) ?
+            (
+              <select onChange={onChangeCategory}>
+                <option value="Quote">Quote</option>
+                <option value="To Everyone">To Everyone</option>
+                <option value="To Staff">To Staff</option>
+                <option value="Meme">Meme</option>
+              </select>
+            ) : ''
+        }
         <input style={inputStyle} type="text" name="name" onChange={onChangeName} value={name} />
         <input style={inputStyle} type="text" name="comment" onChange={onChangeText} value={text} />
         <input type="submit" value="Submit Comment" />
