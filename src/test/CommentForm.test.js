346ca5044a4edad4491f0beeefcdfafabc982769
philcho <Phil Cho>
2017-11-27 12:03:46 -0800

    Create test that checks if Comments form submits correctly.

--- src/test/CommentForm.test.js
+++ src/test/CommentForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import CommentForm from '../components/CommentForm';
 
@@ -9,5 +9,24 @@ describe('Comment Form', () => {
       <CommentForm name="Bob Loblaw" text="Rock on." />
     );
     expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should call onSubmit function upon submit', () => {
+    const onSubmit = jest.fn();
+    const onChangeCommentName = jest.fn();
+    const onChangeCommentText = jest.fn();
+
+    const output = mount(
+      <CommentForm 
+        onSubmit={onSubmit} 
+        onChangeName={onChangeCommentName}
+        onChangeText={onChangeCommentText}
+        name="Bob Loblaw" 
+        text="Rock on." 
+      />
+    );
+    
+    output.find('form').simulate('submit', { preventDefault: jest.fn() });
+    expect(onSubmit.mock.calls.length).toBe(1);
   });
 });
