5f3ea6381146229ab5781e23f2a537234e532fd2
philcho <Phil Cho>
2017-11-27 10:24:36 -0800

    Make Events main page display real items from the db

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 
 const Event = ({event}) => {
   return (
-    // <a className="event container column" href={`/event/${event._id}`}>
-    //   <img className="list-item-img event-img" src={event.picture}/>
-    //   <div className="list-item-name event-name">{event.name}</div>
-    //   <div className="list-item-bio event-bio">{event.description}</div>
-    // </a>
-
-    <a className="event container column" href="###">
-      <img className="list-item-img event-img" src="http://cdn1.theodysseyonline.com/files/2015/12/04/635848643983265445-579762_christmas-puppy-pictures-tzulxzui.jpg"/>
-      <div className="list-item-name event-name">Puppy Christmas</div>
-      <div className="list-item-bio event-bio">There were puppies and we all played and laughed and danced!</div>
+    <a className="event container column" href={`/event/${event._id}`}>
+      <img className="list-item-img event-img" src={event.picture}/>
+      <div className="list-item-name event-name">{event.name}</div>
+      <div className="list-item-bio event-bio">{event.description}</div>
     </a>
   )
 };
